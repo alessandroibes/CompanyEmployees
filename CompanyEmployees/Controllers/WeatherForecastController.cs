07483922ee231ceb9ccf6d79763712b5f4a15645
Alessandro Oliveira <alessandro.oliveira@landisgyr.com>
2021-03-17 22:06:32 -0300

    02 Configuring a Logging Service

--- CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CompanyEmployees.Controllers
 {
@@ -11,29 +8,22 @@ namespace CompanyEmployees.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        private ILoggerManager _logger;
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILoggerManager logger)
         {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<string> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            _logger.LogInfo("Here is info message from our values controller."); 
+            _logger.LogDebug("Here is debug message from our values controller."); 
+            _logger.LogWarn("Here is warn message from our values controller."); 
+            _logger.LogError("Here is an error message from our values controller.");
+
+            return new string[] { "value1", "value2" };
         }
     }
 }
