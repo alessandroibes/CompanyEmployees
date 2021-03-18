90c888628e9cf9bf75011d24fcbea254db88aa66
Alessandro Oliveira <alessandro.oliveira@landisgyr.com>
2021-03-17 21:13:53 -0300

    01 Project Configuration

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// Extension methos to CORS configuration
+        /// </summary>
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()    // WithOrigins("https://example.com")
+                    .AllowAnyMethod()           // WithMethods("POST", "GET")
+                    .AllowAnyHeader());         // WithHeaders("accept", "contenttype") 
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options => { });
+        }
+    }
+}
