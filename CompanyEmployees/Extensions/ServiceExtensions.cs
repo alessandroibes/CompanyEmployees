a8a305287559a4239ab02da44a94cb16f7db45a0
Alessandro Oliveira <alessandro.oliveira@landisgyr.com>
2021-03-19 15:31:02 -0300

    7.6 Implementing a Custom Formatter (CSV)

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Contracts;
+﻿using CompanyEmployees.Formatters;
+using Contracts;
 using Entities;
 using LoggerService;
 using Microsoft.AspNetCore.Builder;
@@ -45,6 +46,12 @@ namespace CompanyEmployees.Extensions
         public static void ConfigureRepositoryManager(this IServiceCollection services)
         {
             services.AddScoped<IRepositoryManager, RepositoryManager>();
+        }
+
+        public static IMvcBuilder AddCustomCSVFormatter(this IMvcBuilder builder)
+        {
+            return builder.AddMvcOptions(config => 
+                config.OutputFormatters.Add(new CsvOutputFormatter()));
         }
     }
 }
