065329f3e5ae7b8cc1131145f015fa21f9df9b9d
Alessandro Oliveira <alessandro.oliveira@landisgyr.com>
2021-03-20 23:41:01 -0300

    15.6 Dependency Injection in Action Filters

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -47,6 +47,8 @@ namespace CompanyEmployees
             });
 
             services.AddScoped<ValidationFilterAttribute>();
+            services.AddScoped<ValidateCompanyExistsAttribute>();
+            services.AddScoped<ValidateEmployeeForCompanyExistsAttribute>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
