a6b446f8e3dda2641265d0c71bcf7b1d15e6e799
Alessandro Oliveira <alessandro.oliveira@landisgyr.com>
2021-03-25 18:51:45 -0300

    22 Working with OPTIONS and HEAD Requests

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -131,5 +131,12 @@ namespace CompanyEmployees.Controllers
 
             return NoContent();
         }
+
+        [HttpOptions] public IActionResult GetCompaniesOptions()
+        {
+            Response.Headers.Add("Allow", "GET, OPTIONS, POST");
+            
+            return Ok();
+        }
     }
 }
