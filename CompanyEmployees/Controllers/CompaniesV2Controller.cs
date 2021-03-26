e45a872e7d5fd4f1b6588ae7e8a32ecab614c9ca
Alessandro Oliveira <alessandro.oliveira@landisgyr.com>
2021-03-26 10:53:44 -0300

    24.2.4 Deprecating Version

--- CompanyEmployees/Controllers/CompaniesV2Controller.cs
+++ CompanyEmployees/Controllers/CompaniesV2Controller.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace CompanyEmployees.Controllers
 {
-    [ApiVersion("2.0")]
+    [ApiVersion("2.0", Deprecated = true)]
     [Route("api/companies")]
     [ApiController]
     public class CompaniesV2Controller : ControllerBase
